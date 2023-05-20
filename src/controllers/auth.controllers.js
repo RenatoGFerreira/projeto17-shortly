fc234749288593e0e7d89503942dfd4c0a60224f
RenatoUbuntu <renatoyferreira@gmail.com>
2023-05-20 19:00:03 -0300

    refactor signin

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,27 +1,53 @@
-import { createUser, userExist } from "../repositories/auth.repository.js";
-import bcrypt from "bcrypt"
+import { db } from "../database/database.js";
+import bcrypt from "bcrypt";
+import { v4 as uuid} from "uuid";
 
 export async function signUp(req, res) {
-  const { name, email, password, confirmPassword } = res.locals.user
-  
+  const { name, email, password } = req.body;
+
   try {
-    const isUserExist = await userExist(email)
-    if(isUserExist){
-      return res.sendStatus(409)
-    }
-    const passwordHash = bcrypt.hashSync(password, 10)
-    const user = {name, email, password:passwordHash}
-    await createUser(user)
-    res.sendStatus(201)
+    const isUserExist = await db.query(
+      `
+    SELECT * FROM users WHERE email =$1;
+    `,
+      [email]
+    );
     
+    if (isUserExist.rowCount > 0) return res.sendStatus(409);
+
+    const passwordHash = bcrypt.hashSync(password, 10);
+
+    await db.query(`
+      INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3);
+    `, [name, email, passwordHash])
+
+    res.sendStatus(201);
   } catch (err) {
     res.status(500).send(err.message);
   }
 }
 
 export async function signIn(req, res) {
-  try {
-  } catch (err) {
-    res.status(500).send(err.message);
+  const { email, password } = req.body
+
+  const { rows: user} = await db.query(`
+    SELECT * FROM users WHERE email=$1
+  `, [email]
+  )
+
+  if(!user) return res.sendStatus(401)
+
+  if(bcrypt.compareSync(password, user[0].password)) {
+    const token = uuid()
+
+    await db.query(`
+      INSERT INTO sessions (token, "userId")
+      VALUES ($1, $2)
+    `, [token, user[0].id])
+    
+    return res.send({token})
   }
+
+  return res.sendStatus(401)
 }
--- src/middlewares/validateSchema.middleware.js
+++ src/middlewares/validateSchema.middleware.js
@@ -1,13 +1,11 @@
 export function validateSchema(schema){
     return (req, res, next) =>{
-        const user = req.body
         
         const validation = schema.validate(user, {abortEarly: false})
         if(validation.error){
             return res.status(422).send(validation.error.details.map(detail => detail.message))
         }
 
-        res.locals.user = user
         next()
     }   
 }
